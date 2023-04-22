7925c0ab075d9d35e0eefcc4823d470dc257c54c
abc2513 <2513151911@qq.com>
2023-04-22 23:27:12 +0800

    创建项目

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: abc2513 2513151911@qq.com
+ * @Date: 2023-04-22 23:06:49
+ * @LastEditors: abc2513 2513151911@qq.com
+ * @LastEditTime: 2023-04-22 23:15:41
+ * @FilePath: \fruit-website\src\pages\IndexPage.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react'
+
+export default function IndexPage() {
+  return (
+    <div>IndexPage</div>
+  )
+}
